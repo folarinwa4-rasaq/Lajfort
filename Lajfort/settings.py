808a8ee8b706db1db4d6777a74d6e01f45a107fc
Rasaq Folarinwa <rasaqfolarinwa@gmail.com>
2024-10-06 20:56:05 -0700

    fourth changes

--- Lajfort/settings.py
+++ Lajfort/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'Lajfort.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': r'C:\Users\USER\Desktop\DataBases\lajfortdb.sqlite3',
     }
 }
-
+# BASE_DIR / 'db.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
