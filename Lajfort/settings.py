e8b441397691939403584444e7b83fdaf98cf553
agbadevemma <agbadevemmanuel@gmail.com>
2025-01-13 21:27:53 +0100

    added custom domain

--- Lajfort/settings.py
+++ Lajfort/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY'),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lajfort.onrender.com', 'lajfortschools.com']
+ALLOWED_HOSTS = ['lajfort.onrender.com','www.lajfortschools.com', 'lajfortschools.com']
 
 
 # Application definition
