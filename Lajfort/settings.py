4c11f873f3e2c1b0c3761cd5a2185360e11bd62b
agbadevemma <agbadevemmanuel@gmail.com>
2025-01-13 20:59:06 +0100

    added new items to git

--- Lajfort/settings.py
+++ Lajfort/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,11 +77,13 @@ WSGI_APPLICATION = 'Lajfort.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+       default=config('DATABASE_URL'),
+        conn_max_age=600,  # Optional: Reuse connections
+        ssl_require=True   # Enforce SSL for Render-hosted databases
+    )
 }
 #r'C:\Users\USER\OneDrive\Desktop\DataBases\lajfortdb'
 # BASE_DIR / 'db.sqlite3'
