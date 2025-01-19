dc853860641134f69bd4c6d4fc2a6749fec57915
agbadevemma <agbadevemmanuel@gmail.com>
2025-01-19 22:23:58 +0100

    done

--- Lajfort/settings.py
+++ Lajfort/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY'),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','lajfort.onrender.com','www.lajfortschools.com', 'lajfortschools.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','lajfort.onrender.com','www.lajfortschools.com', 'lajfortschools.com']
 
 
 # Application definition
